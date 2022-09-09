190563dc20d7b90de5549b9a4472721a6b1a9980
Johnny <nor0x@users.noreply.github.com>
2022-09-09 10:08:21 +0200

    temp commit ⏲️

--- OverFlower/OverFlower.cs
+++ OverFlower/OverFlower.cs
@@ -3,13 +3,27 @@ using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Animation = Microsoft.Maui.Controls.Animation;
+using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+#if WINDOWS10_0_17763_0_OR_GREATER
+using Border = Microsoft.Maui.Controls.Border;
+using Image = Microsoft.Maui.Controls.Image;
+using Grid = Microsoft.Maui.Controls.Grid;
+using Microsoft.UI.Xaml.Controls;
+
+#endif
 #endif
 
 #if FORMS
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 #endif
 using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace OverFlower;
 
@@ -19,6 +33,8 @@ public class OverFlower : Border
 #if FORMS
 public class OverFlower : Frame
 #endif
+
+
 {
 
     public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(OverFlower));
@@ -30,8 +46,6 @@ public class OverFlower : Frame
     public static readonly BindableProperty ScrollDurationProperty = BindableProperty.Create(nameof(ScrollDuration), typeof(double), typeof(OverFlower), default(double));
 
     public static readonly BindableProperty ReverseProperty = BindableProperty.Create(nameof(Reverse), typeof(bool), typeof(OverFlower), default(bool));
-
-    public static readonly BindableProperty MirrorProperty = BindableProperty.Create(nameof(Mirror), typeof(bool), typeof(OverFlower), default(bool));
 
     public static readonly BindableProperty ScrollDirectionProperty = BindableProperty.Create(nameof(ScrollDirection), typeof(ScrollDirection), typeof(OverFlower), ScrollDirection.Left, propertyChanged: OnScrollDirectionChanged);
 
@@ -60,12 +74,6 @@ public class OverFlower : Frame
         set { SetValue(ReverseProperty, value); }
     }
 
-    public bool Mirror
-    {
-        get { return (bool)GetValue(MirrorProperty); }
-        set { SetValue(MirrorProperty, value); }
-    }
-
     public ScrollDirection ScrollDirection
     {
         get { return (ScrollDirection)GetValue(ScrollDirectionProperty); }
@@ -85,101 +93,123 @@ public class OverFlower : Frame
     double _spaceEliminator = 1;
     int _initialZ;
 
-    double _width;
-    double _height;
     public OverFlower()
     {
-        _container = new()
+#if MAUI
+        Background = Colors.BlueViolet;
+#endif
+        //TODO:
+        //add code to repeat images if they are smaller than the container
+        Content = new Grid
         {
-            WidthRequest = WidthRequest,
-            HeightRequest = HeightRequest,
 #if MAUI
             Background = Colors.Maroon,
 #endif
 #if FORMS
             Background = Color.Maroon,
 #endif
-            IsClippedToBounds = true,
-        };
-        Content = _container;
+            //Clip = new RectangleGeometry(new Rect(0, 0, 400, 500)),
+            Children =
+            {
+                new Image
+                {
+                    Source = "first.jpg",
 #if MAUI
-        Loaded += OverFlower_Loaded;
+                    WidthRequest = 987,
+                    HeightRequest = 1480,
+                    Aspect = Aspect.AspectFit,
+                    Background = Colors.Magenta,
 #endif
+                },
+
+
+                new Image
+                {
+                    Source = "second.jpg",
+                    WidthRequest = 987,
+                    HeightRequest = 1480,
+#if MAUI
+                    Aspect = Aspect.AspectFit,
+                    Background = Colors.CornflowerBlue,
+#endif
+                }
+            }
+        };
+#if MAUI
+        _container = (Grid)Content;
+        _first = (Image)_container.Children.First();
+        _second = (Image)_container.Children.Last();
+#endif
+
+
+#if MAUI
+        _initialZ = _first.ZIndex;
+#endif
+
+
+#if MAUI
+        Background = Colors.GreenYellow;
+#endif
+
+
 #if FORMS
         HasShadow = false;
         Padding = 0;
         CornerRadius = 0;
         Margin = 0;
-        SizeChanged += OverFlower_Loaded;
 #endif
     }
-
-    private void OverFlower_Loaded(object sender, EventArgs e)
+    
+    public void Animat()
     {
-        Init();
-    }
+        Debug.WriteLine(_first.GetInfo());
 
-    void Init()
-    {
-        //TODO:
-        //add code to repeat images if they are smaller than the container
-        _first = new Image
-        {
-            Source = "first.jpg",
-            WidthRequest = ImageWidth <= this.Width ? this.Width : ImageWidth,
-            HeightRequest = ImageHeight <= this.Height ? this.Height : ImageHeight,
-            Aspect = Aspect.AspectFit,
-#if MAUI
-            Background = Colors.Magenta,
-#endif
-        };
-        _second = new Image
-        {
-            Source = "second.jpg",
-            WidthRequest = ImageWidth <= this.Width ? this.Width : ImageWidth,
-            HeightRequest = ImageHeight <= this.Height ? this.Height : ImageHeight,
-            Aspect = Aspect.AspectFit,
+        var x = Extensions.GeneratePropertiesDictionary(this);
+        var y = Extensions.GeneratePropertiesDictionary(_container);
+        var z = Extensions.GeneratePropertiesDictionary(_first);
 
-#if MAUI
-            Background = Colors.CornflowerBlue,
-#endif
-        };
+#if WINDOWS10_0_17763_0_OR_GREATER
 
-        if (Mirror)
+
+        Debug.WriteLine("OVERFLOWER:");
+        if (Handler is BorderHandler bh && bh.PlatformView is Panel p)
         {
-            if (ScrollDirection == ScrollDirection.Up || ScrollDirection == ScrollDirection.Down)
+            foreach(var c in p.Children)
             {
-                _second.RotationX = 180;
+                Debug.WriteLine("c: " + c.GetType());
+                if(c is LayoutPanel pa && pa.Children is UIElementCollection uec)
+                {
+                    foreach(var ue in uec)
+                    {
+                        Debug.WriteLine("uec: " + ue.GetType());
+                    }
+                }
             }
-            if (ScrollDirection == ScrollDirection.Left || ScrollDirection == ScrollDirection.Right)
+            if (_container.Handler is LayoutHandler lh && lh.PlatformView is LayoutPanel g)
             {
-                _second.RotationY = 180;
+                g.Clip = null;
             }
+            if (_first.Handler is ImageHandler ih && ih.PlatformView is Microsoft.UI.Xaml.Controls.Image i)
+            {
+                i.Clip = null;
+            }
+            if (_second.Handler is ImageHandler ih2 && ih2.PlatformView is Microsoft.UI.Xaml.Controls.Image i2)
+            {
+                i2.Clip = null;
+            }
+            p.Clip = null;
         }
 
-#if MAUI
-
-#endif
-
-        _container.Children.Add(_first);
-        _container.Children.Add(_second);
-        Debug.WriteLine("first: " + _first.WidthRequest + " " + _first.HeightRequest);
-        Debug.WriteLine("secondns: " + _second.WidthRequest + " " + _second.HeightRequest);
-#if MAUI
-        _initialZ = _first.ZIndex;
 #endif
 
         InitialOffset();
+        
         StartAnimation();
-
     }
 
     void InitialOffset()
     {
-#if MAUI
-        Debug.WriteLine("initial offset WIDTH: " + _second.Width + " REQUEST: " + _second.WidthRequest + " DESIRED: " + _second.DesiredSize.Width);
-        Debug.WriteLine("initial offset HEIGHT: " + _second.Height + " REQUEST: " + _second.HeightRequest + " DESIRED: " + _second.DesiredSize.Height);
-#endif
+
         //initial offset
         _first.TranslationX = 0;
         _first.TranslationY = 0;
@@ -188,7 +218,7 @@ public class OverFlower : Frame
             case ScrollDirection.Left:
                 _second.TranslationY = 0;
 #if MAUI
-                _second.TranslationX = -_second.DesiredSize.Width;
+                _second.TranslationX = -_second.WidthRequest;
 #endif
 #if FORMS
                 _second.TranslationX = -_second.Width;
@@ -198,7 +228,7 @@ public class OverFlower : Frame
             case ScrollDirection.Up:
                 _second.TranslationX = 0;
 #if MAUI
-                _second.TranslationY = -_second.DesiredSize.Height;
+                _second.TranslationY = -_second.HeightRequest;
 #endif
 #if FORMS
                 _second.TranslationY = -_second.Height;
@@ -208,7 +238,7 @@ public class OverFlower : Frame
             case ScrollDirection.Right:
                 _second.TranslationY = 0;
 #if MAUI
-                _second.TranslationX = -_second.DesiredSize.Width;
+                _second.TranslationX = -_second.WidthRequest;
 #endif
 #if FORMS
                 _second.TranslationX = -_second.Width;
@@ -218,7 +248,7 @@ public class OverFlower : Frame
             case ScrollDirection.Down:
                 _second.TranslationX = 0;
 #if MAUI
-                _second.TranslationY = -_second.DesiredSize.Height;
+                _second.TranslationY = -_second.HeightRequest;
 #endif
 #if FORMS
                 _second.TranslationY = -_second.Height;
@@ -226,26 +256,27 @@ public class OverFlower : Frame
 #endif
                 break;
         }
-
-        Debug.WriteLine("SECOND OFFSET: " + _second.TranslationX + " " + _second.TranslationY);
     }
 
     void StartAnimation()
     {
         this.AbortAnimation("scrolling");
+#if MAUI
+        Debug.WriteLine("first: z " + _first.ZIndex + " " + _second.ZIndex);
 
+#endif
         Action<double> callback = null;
         switch (ScrollDirection)
         {
             case ScrollDirection.Left:
 #if MAUI
-                _first.ZIndex = _second.ZIndex;
-                _second.ZIndex = _initialZ;
+                //_first.ZIndex = _second.ZIndex;
+                //_second.ZIndex = _initialZ;
 #endif
                 callback = new Action<double>((d) =>
                 {
 #if MAUI
-                    var width = _first.DesiredSize.Width;
+                    var width = _first.WidthRequest;
 #endif
 #if FORMS
                     var width = _first.Width;
@@ -253,24 +284,25 @@ public class OverFlower : Frame
                     var transX = -width * d;
                     _first.TranslationX = transX;
                     _second.TranslationX = transX + width - _spaceEliminator;
-                    Debug.WriteLine("left first " + _first.RotationY);
-                    Debug.WriteLine("left second " + _second.RotationY);
+
 
                 });
                 break;
             case ScrollDirection.Up:
 #if MAUI
-                _first.ZIndex = _second.ZIndex;
-                _second.ZIndex = _initialZ;
+                //_first.ZIndex = _second.ZIndex;
+                //_second.ZIndex = _initialZ;
 #endif
                 callback = new Action<double>((d) =>
                 {
 #if MAUI
-                    var height = _first.DesiredSize.Height;
+                    var height = _first.HeightRequest;
 #endif
 #if FORMS
                     var height = _first.Height;
 #endif
+                    Debug.WriteLine("over D: " + d.ToString("0.00"));
+
                     var transY = -height * d;
                     _first.TranslationY = transY;
                     _second.TranslationY = transY + height - _spaceEliminator;
@@ -278,30 +310,30 @@ public class OverFlower : Frame
                 break;
             case ScrollDirection.Right:
 #if MAUI
-                _first.ZIndex = _second.ZIndex + 1;
+                //_first.ZIndex = _second.ZIndex + 1;
 #endif
                 callback = new Action<double>((d) =>
                 {
 #if MAUI
-                    var width = _first.DesiredSize.Width;
+                    var width = _first.WidthRequest;
 #endif
 #if FORMS
                     var width = _first.Width;
 #endif
+
                     var transX = width * d;
                     _first.TranslationX = transX;
                     _second.TranslationX = transX - width + _spaceEliminator;
-                    Debug.WriteLine("right");
                 });
                 break;
             case ScrollDirection.Down:
 #if MAUI
-                _first.ZIndex = _second.ZIndex + 1;
+                //_first.ZIndex = _second.ZIndex + 1;
 #endif
                 callback = new Action<double>((d) =>
                 {
 #if MAUI
-                    var height = _first.DesiredSize.Height;
+                    var height = _first.HeightRequest;
 #endif
 #if FORMS
                     var height = _first.Height;
@@ -315,13 +347,13 @@ public class OverFlower : Frame
                 callback = new Action<double>((d) =>
                 {
 #if MAUI
-                _first.ZIndex = _second.ZIndex;
-                _second.ZIndex = _initialZ;
+                //_first.ZIndex = _second.ZIndex;
+                //_second.ZIndex = _initialZ;
 #endif
                     callback = new Action<double>((d) =>
                     {
 #if MAUI
-                    var width = _first.DesiredSize.Width;
+                    var width = _first.WidthRequest;
 #endif
 #if FORMS
                         var width = _first.Width;
@@ -339,31 +371,6 @@ public class OverFlower : Frame
         {
             if (!hasFinished)
             {
-                if(Mirror)
-                {
-                    if (ScrollDirection == ScrollDirection.Up || ScrollDirection == ScrollDirection.Down)
-                    {
-                        _first.RotationX = 180;
-                        _second.RotationX = 0;
-                    }
-                    if (ScrollDirection == ScrollDirection.Left || ScrollDirection == ScrollDirection.Right)
-                    {
-                        Debug.WriteLine("HAS ENDED: " + _first.RotationY + " " + _second.RotationY);
-                        if(_first.RotationY == 0)
-                        {
-                            _first.RotationY = 180;
-                            _second.RotationY = 0;
-                        }
-                        else if(_first.RotationY == 180)
-                        {
-                            _first.RotationY = 0;
-                            _second.RotationY = 180;
-                        }
-
-                    }
-                    InitialOffset();
-                    StartAnimation();
-                }
                 if (Reverse)
                 {
                     switch (ScrollDirection)
@@ -384,21 +391,7 @@ public class OverFlower : Frame
                 }
             }
         },
-        repeat: () =>
-        {
-            if(Reverse)
-            {
-                return false;
-            }
-            else
-            {
-                if(Mirror)
-                {
-                    return false;
-                }
-                return true;
-            }
-        });
+        repeat: () => !Reverse);
     }
 
 
@@ -408,8 +401,8 @@ public class OverFlower : Frame
         {
             if (overflower._first != null)
             {
-                overflower.InitialOffset();
-                overflower.StartAnimation();
+                //overflower.InitialOffset();
+                //overflower.StartAnimation();
             }
         }
     }
@@ -421,4 +414,29 @@ public enum ScrollDirection
     Down,
     Left,
     Right
+}
+
+public static class Extensions
+{
+    public static List<string> GeneratePropertiesDictionary(object myClass)
+    {
+        return myClass.GetType()
+                      .GetProperties()
+                      .Where(p => p.GetIndexParameters().Length == 0)
+                      .Select(p => p.Name + " " + p.GetValue(myClass))
+                      .ToList();
+    }
+
+    public static string GetInfo(this Image img)
+    {
+        return "Width: " + img.Width + System.Environment.NewLine +
+                "Height: " + img.Height + System.Environment.NewLine +
+                "WidthRequest: " + img.WidthRequest + System.Environment.NewLine +
+                "HeightRequest: " + img.HeightRequest + System.Environment.NewLine +
+                "Bounds: " + img.Bounds + System.Environment.NewLine +
+#if MAUI
+                "Frame: " + img.Frame + System.Environment.NewLine +
+#endif
+                "LayoutOptions: " + img.HorizontalOptions.Alignment + img.HorizontalOptions.Expands + img.VerticalOptions.Alignment + img.VerticalOptions.Expands + System.Environment.NewLine;
+    }
 }
--- SampleMaui/MainPage.xaml.cs
+++ SampleMaui/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using OverFlower;
+﻿using Microsoft.Maui.Controls;
+using OverFlower;
+using System.Diagnostics;
 
 namespace SampleMaui
 {
@@ -9,20 +11,102 @@ namespace SampleMaui
         {
             InitializeComponent();
             _rand = new();
+            AddMarkupBorder();
+            myimg.Loaded += Myimg_Loaded;
+            myimg.PropertyChanged += Myimg_PropertyChanged;
+            
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void Myimg_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            /*
-            var values = Enum.GetValues(typeof(ScrollDirection));
-            var scrollDir = (ScrollDirection)values.GetValue(_rand.Next(values.Length));
-            MyFlower.ScrollDirection = scrollDir;
-            MyFlower.Reverse = _rand.NextDouble() > 0.5;
-            MyFlower.ScrollDuration = _rand.Next(100, 6000);
-            */
-            myimg.Rotation = Convert.ToDouble(zValue.Text);
-            myimg.RotationX = Convert.ToDouble(xValue.Text);
-            myimg.RotationY = Convert.ToDouble(yValue.Text);
+            Debug.WriteLine("XAML property changed: " + e.PropertyName);
+        }
+
+        private void Myimg_Loaded(object? sender, EventArgs e)
+        {
+            Debug.WriteLine("XAML LOADED: \n" + myimg.GetInfo());
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var x = OverFlower.Extensions.GeneratePropertiesDictionary(myborder);
+            var y = OverFlower.Extensions.GeneratePropertiesDictionary(mygrid);
+            var z = OverFlower.Extensions.GeneratePropertiesDictionary(myimg);
+
+            var ani1 = new Animation((d) =>
+            {
+                var height = myimg.HeightRequest;
+
+                var transY = height * d;
+                myimg.TranslationY = transY;
+                myimg2.TranslationY = transY - height;
+            }, 0, 1, Easing.Linear);
+            
+            ani1.Commit(this, "helloworld", 14, 4000, Easing.Linear);
+
+            var ani2 = new Animation((d) =>
+            {
+                var height = _markupImg1.HeightRequest;
+
+                double transY = height * d;
+                _markupImg1.TranslationY = transY;
+                _markupImg2.TranslationY = transY - height;
+            }, 0, 1, Easing.Linear);
+
+            ani2.Commit(this, "hellohworld", 14, 4000, Easing.Linear);
+
+
+            MyFlower.Animat();
+            mauiflower.Start();
+
+        }
+        Image _markupImg1;
+        Image _markupImg2;
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            mylabel.Text = "mylabel: " + mylabel.ZIndex;
+        }
+
+        void AddMarkupBorder()
+        {
+            var border = new Border()
+            {
+                WidthRequest = 400,
+                HeightRequest = 500,
+                StrokeThickness = 0,
+                Background = Colors.LightGreen,
+                Content = new Grid()
+                {
+                    Children =
+                    {
+                        new Image
+                        {
+                            Source = "first.jpg",
+                            WidthRequest = 987,
+                            HeightRequest = 1480,
+                            Aspect = Aspect.AspectFit,
+                            Background = Colors.Magenta,
+                        },
+                        new Image
+                        {
+                            Source = "second.jpg",
+                            WidthRequest = 987,
+                            HeightRequest = 1480,
+                            Aspect = Aspect.AspectFit,
+                            TranslationY = -1480,
+                            Background = Colors.Magenta,
+                        },
+                    }
+                }
+            };
+            if(border.Content is Grid g && g.Children.First() is Image fi && g.Children.Last() is Image la)
+            {
+                _markupImg1 = fi;
+                _markupImg2 = la;
+            }
+            outgrid.Add(border, 0, 0);
         }
     }
 }
